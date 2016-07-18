633ba77584ba67388758ecb06961cb2b3da3342f
usuario <usuario@equipo>
2016-07-18 11:16:28 -0500

    colocar el comentario eliminado

--- src/proyectodeprograma/Utilidades.java
+++ src/proyectodeprograma/Utilidades.java
@@ -26,6 +26,10 @@ public class Utilidades {
         dato=dato.toLowerCase();
         return dato;
     }
+    /**
+     *  Método encargado de leer un dato de tipo entero.
+     * @return dato int.
+     */
     public static int leerInt(){
         Scanner teclado = new Scanner(System.in);
         int dato;
